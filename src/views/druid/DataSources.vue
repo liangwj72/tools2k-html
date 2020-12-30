9ed2ed80754b20376479ed8bd75d289f7a74d939
梁韦江 <liangwj72@qq.com>
2020-12-30 10:56:49 +0800

    修复数据源不能切换的bug

--- src/views/druid/DataSources.vue
+++ src/views/druid/DataSources.vue
@@ -13,13 +13,13 @@
     </div>
 
     <el-tabs type="border-card"
-             :value="activeTab"
+             :value.sync="activeTab"
              @tab-click="onTabClick">
       <el-tab-pane
         v-for="(row,index) in list"
         :key="index"
         :name="''+row.Identity"
-        :label="row.Name">
+        :label="row.DbType + '-' + row.Identity">
 
         <!-- 数据源的基础信息 -->
         <el-card class="article-small-card">
@@ -184,96 +184,7 @@
     /** 本页面的属性 */
     data() {
       return {
-        list: [
-          {
-            "Identity": 947982595,
-            "Name": "DataSource-947982595",
-            "DbType": "oracle",
-            "DriverClassName": "oracle.jdbc.OracleDriver",
-            "URL": "jdbc:oracle:thin:@172.16.3.68:21521/test_nnbdc_gbk",
-            "UserName": "bdck",
-            "FilterClassNames": [
-              "com.alibaba.druid.filter.stat.StatFilter"
-            ],
-            "WaitThreadCount": 0,
-            "NotEmptyWaitCount": 1,
-            "NotEmptyWaitMillis": 0,
-            "PoolingCount": 0,
-            "PoolingPeak": 1,
-            "PoolingPeakTime": 1609200521641,
-            "ActiveCount": 0,
-            "ActivePeak": 1,
-            "ActivePeakTime": 1609200521644,
-            "InitialSize": 0,
-            "MinIdle": 0,
-            "MaxActive": 8,
-            "QueryTimeout": 0,
-            "TransactionQueryTimeout": 0,
-            "LoginTimeout": 0,
-            "ValidConnectionCheckerClassName": "com.alibaba.druid.pool.vendor.OracleValidConnectionChecker",
-            "ExceptionSorterClassName": "com.alibaba.druid.pool.vendor.OracleExceptionSorter",
-            "TestOnBorrow": true,
-            "TestOnReturn": false,
-            "TestWhileIdle": true,
-            "DefaultAutoCommit": true,
-            "LogicConnectCount": 1,
-            "LogicCloseCount": 1,
-            "LogicConnectErrorCount": 0,
-            "PhysicalConnectCount": 1,
-            "PhysicalCloseCount": 1,
-            "PhysicalConnectErrorCount": 0,
-            "DiscardCount": 0,
-            "ExecuteCount": 0,
-            "ExecuteUpdateCount": 0,
-            "ExecuteQueryCount": 0,
-            "ExecuteBatchCount": 0,
-            "ErrorCount": 0,
-            "CommitCount": 0,
-            "RollbackCount": 0,
-            "PSCacheAccessCount": 0,
-            "PSCacheHitCount": 0,
-            "PSCacheMissCount": 0,
-            "StartTransactionCount": 0,
-            "TransactionHistogram": [
-              0,
-              0,
-              0,
-              0,
-              0,
-              0,
-              0
-            ],
-            "ConnectionHoldTimeHistogram": [
-              0,
-              0,
-              0,
-              1,
-              0,
-              0,
-              0,
-              0
-            ],
-            "RemoveAbandoned": false,
-            "ClobOpenCount": 0,
-            "BlobOpenCount": 0,
-            "KeepAliveCheckCount": 0,
-            "KeepAlive": false,
-            "FailFast": false,
-            "MaxWait": -1,
-            "MaxWaitThreadCount": -1,
-            "PoolPreparedStatements": false,
-            "MaxPoolPreparedStatementPerConnectionSize": 10,
-            "MinEvictableIdleTimeMillis": 1800000,
-            "MaxEvictableIdleTimeMillis": 25200000,
-            "LogDifferentThread": true,
-            "RecycleErrorCount": 0,
-            "PreparedStatementOpenCount": 0,
-            "PreparedStatementClosedCount": 0,
-            "UseUnfairLock": true,
-            "InitGlobalVariants": false,
-            "InitVariants": false
-          }
-        ],
+        list: [],
 
         activeTab: '',//激活的tab的name，值是datasource的id
 
@@ -319,6 +230,7 @@
         // 当前查看的sql
         detail: {
           visible: false,
+          id: '',
           sql: {
             "ExecuteAndResultSetHoldTime": 280,
             "EffectedRowCountHistogram": [1, 0, 0, 0, 0, 0],
@@ -354,41 +266,7 @@
           },
         },
 
-        sqlList: [
-          {
-            "ExecuteAndResultSetHoldTime": 280,
-            "EffectedRowCountHistogram": [1, 0, 0, 0, 0, 0],
-            "Histogram": [0, 0, 0, 1, 0, 0, 0, 0],
-            "InputStreamOpenCount": 0,
-            "BatchSizeTotal": 0,
-            "FetchRowCountMax": 1,
-            "ErrorCount": 0,
-            "BatchSizeMax": 0,
-            "ReaderOpenCount": 0,
-            "EffectedRowCountMax": 0,
-            "InTransactionCount": 0,
-            "ResultSetHoldTime": 13,
-            "TotalTime": 266,
-            "ID": 1,
-            "ConcurrentMax": 1,
-            "RunningCount": 0,
-            "FetchRowCount": 1,
-            "MaxTimespanOccurTime": 1609213373382,
-            "ReadBytesLength": 0,
-            "DbType": "oracle",
-            SQL: "SELECT     ZD.ZDDM, ZD.ZL, ZD.ZDMJ, ZD.ZDSZD, ZD.ZDSZN, ZD.ZDSZX, ZD.ZDSZB, ZD.TFH,    ZD.RJL, ZD.JZMD, ZD.JZXG, ZD.QXMC, ZD.DJQMC, ZD.DJZQMC, ZD.BZ,     (SELECT Z.ZSBH FROM BDCK.BDCS_ZS_GZ Z         INNER JOIN BDCK.BDCS_QDZR_XZ L ON Z.ZSID = L.ZSID         INNER JOIN BDCK.BDCS_QL_XZ Q ON L.QLID = Q.QLID         INNER JOIN BDCK.BDCS_DJDY_XZ D ON Q.QLLX = '3'  AND Q.DJDYID = D.DJDYID         WHERE  D.BDCDYID = ZD.BDCDYID ) TDZBH,     ( SELECT LISTAGG ( TO_CHAR ( TDYTMC ), ',' ) within GROUP ( ORDER BY 1 )         FROM BDCK.BDCS_TDYT_LS WHERE BDCDYID = zd.bdcdyid ) TDYT,     ( SELECT CONSTTRANS FROM BDCK.BDCS_CONST         WHERE CONSTSLSID = '9' AND CONSTVALUE = zd.qlxz ) SYQZL,     CASE WHEN EXISTS ( SELECT BDCDYID FROM BDCK.BDCS_H_XZ         WHERE ZDBDCDYID = ZD.BDCDYID         UNION ALL SELECT BDCDYID FROM BDCK.BDCS_H_XZY WHERE ZDBDCDYID = ZD.BDCDYID)         THEN '有房屋' ELSE '无房屋' END FWZT, CASE WHEN EXISTS (SELECT qlid             FROM BDCK.BDCS_QL_XZ Q             INNER JOIN BDCK.BDCS_DJDY_XZ D ON Q.QLLX = '23' AND Q.QLID = D.DJDYID             WHERE D.BDCDYID = ZD.BDCDYID )         THEN '有抵押' ELSE '无抵押' END TDDYZT FROM BDCK.BDCS_SHYQZD_XZ ZD     WHERE  ZDDM = ?",
-            "HASH": 216127831708071550,
-            "MaxTimespan": 266,
-            "BlobOpenCount": 0,
-            "ExecuteCount": 1,
-            "EffectedRowCount": 0,
-            "ReadStringLength": 129,
-            "ExecuteAndResultHoldTimeHistogram": [1, 0, 0, 0, 0, 0, 0, 0],
-            "ClobOpenCount": 0,
-            "LastTime": 1609213373115,
-            "FetchRowCountHistogram": [0, 1, 0, 0, 0, 0]
-          }
-        ]
+        sqlList: []
       }
     },
 
@@ -412,6 +290,7 @@
           this.list = res.list
           if (res.list.length > 0) {
             this.activeTab = '' + res.list[0].Identity
+            this.detail.id = this.activeTab
             this.reloadSqlList()
           }
         })
@@ -419,15 +298,16 @@
 
       /** 重新加载 数据源的sql */
       reloadSqlList() {
-        console.debug("reloadSqlList", this.activeTab)
-        jsLib.ajax.post(apiUrl.commonDurid.getSqlByDatasourceId, {id: this.activeTab}).then((res) => {
+        console.debug("reloadSqlList", this.detail.id)
+        jsLib.ajax.post(apiUrl.commonDurid.getSqlByDatasourceId, {id: this.detail.id}).then((res) => {
           // console.debug(`获取了Druid连接池的数据源 ${this.activeTab} 的sql列表`, res.list)
           this.sqlList = res.list
         })
       },
 
-      onTabClick() {
-        // console.debug("onTabClick", tabObj.name)
+      onTabClick(tabObj) {
+        console.debug("onTabClick", tabObj.name)
+        this.detail.id = tabObj.name
         this.reloadSqlList()
       },
 
