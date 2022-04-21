6cb975254cf6bf36525dde83e4120f1d215b9f26
梁韦江 <liangwj72@qq.com>
2022-04-21 18:05:20 +0800

    修复发版时静态资源路径问题

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
-const path = require('path');
+const path = require('path')
 const {defineConfig} = require('@vue/cli-service')
+
+/** 发版时静态资源的路径前缀 */
+const assetsPrefix = '_common_/statics/admin/'
 
 /** 是否热更新，为false的时候，页面是刷新，而不是动态更新，可避免动画卡死的现象 */
 const hotReload = true
@@ -15,7 +18,7 @@ function myProxy() {
 
     // 以下url要转发到后端
     const urls = [
-        '/api/', '/upload/','/_common_/'
+        '/api/', '/upload/', '/_common_/',
     ]
 
     // 迭代urls，生成代理规则
@@ -26,7 +29,7 @@ function myProxy() {
             target: process.env.VUE_APP_BACKEND_SERVER,
             ws: true,
             changeOrigin: true,
-            secure: process.env.DEV_SERVER_HTTPS === 'true' // 如果是https接口，需要配置为 true
+            secure: process.env.DEV_SERVER_HTTPS === 'true', // 如果是https接口，需要配置为 true
         }
     }
     console.log('当前用的API代理', process.env.VUE_APP_BACKEND_SERVER, proxy)
@@ -37,19 +40,21 @@ module.exports = defineConfig({
     transpileDependencies: true,
     outputDir: '../lib-java/spring-support/src/main/resources/commons_statics/admin/',
 
+    publicPath: process.env.NODE_ENV === 'production' ? assetsPrefix : '/',
+
     chainWebpack: config => {
         config.module
             .rule('vue')
             .use('vue-loader')
             .loader('vue-loader')
             .tap(options => {
-                options.hotReload = hotReload;
+                options.hotReload = hotReload
                 return options
             })
     },
 
     devServer: {
         hot: hotReload,
-        proxy: myProxy()
+        proxy: myProxy(),
     },
 })
